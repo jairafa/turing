e9b82b4a686efafce7b2496941dad61f676e29ec
Jairo Vega <jairafa@gmail.com>
2022-03-10 16:06:19 -0500

    Se agregan proyectos para verificar lineas de código y se prueba en models

--- cliente/models.py
+++ cliente/models.py
@@ -3,39 +3,98 @@ from django.contrib.auth.models import User
 
 
 class category(models.Model):
-	name = models.CharField(max_length=50)
-	created_at = models.DateTimeField(auto_now_add=True) 
-	updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(
+        "Fecha de creación", auto_now_add=True, auto_now=False
+    )
+    updated_at = models.DateTimeField(
+        "Fecha de actualización", auto_now_add=False, auto_now=True
+    )
 
+    class Meta:
+        ordering = ["name"]
 
-	class Meta:
-		ordering = ['name']
+    def __str__(self):
+        return "{} ".format(self.name)
+
 
 class territorial(models.Model):
-	name = models.CharField(max_length=50)
-	parent = models.ForeignKey('self', models.DO_NOTHING,related_name='dad', blank=False, null=False)
-	territorial_list = (('country', 'pais',),('city', 'ciudad',))
-	territorial_type = models.CharField(max_length=12, choices=territorial_list, default='city')
-	created_at = models.DateTimeField(auto_now_add=True) 
-	updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey(
+        "self", models.DO_NOTHING, related_name="dad", blank=True, null=True
+    )
+    territorial_list = (
+        (
+            "P",
+            "Pais",
+        ),
+        (
+            "D",
+            "Departamento",
+        ),
+        (
+            "C",
+            "Ciudad",
+        ),
+    )  # noqa
+    territorial_type = models.CharField(
+        "Tipo Territorio", max_length=12, choices=territorial_list, default="city"
+    )
+    created_at = models.DateTimeField(
+        "Fecha de creación", auto_now_add=True, auto_now=False
+    )
+    updated_at = models.DateTimeField(
+        "Fecha de actualización", auto_now_add=False, auto_now=True
+    )
 
-	class Meta:
-		ordering = ['name']
+    class Meta:
+        ordering = ["name"]
 
-	def __str__(self):
-		return '{} {} {}'.format(self.id, self.name, self.territorial_type)
+    def __str__(self):
+        return "{} {} {} padre {}".format(
+            self.id, self.name, self.territorial_type, self.parent_id
+        )
+
 
 class cliente(models.Model):
-	name = models.CharField(max_length=50)
-	country = models.ForeignKey(territorial, models.DO_NOTHING,related_name='pais', blank=False, null=False)
-	city = models.ForeignKey(territorial,models.DO_NOTHING,related_name='ciudad', blank=False, null=False)
-	category = models.ForeignKey(category, models.DO_NOTHING, blank=False, null=False) 
-	user_created = models.ForeignKey(User, models.DO_NOTHING) 
-	active = (('1', 'activo',),('0', 'inactivo',))
-	is_active = models.CharField(max_length=1, choices=active, default='1')
-	created_at = models.DateTimeField(auto_now_add=True) 
-	updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+    country = models.ForeignKey(
+        territorial, models.DO_NOTHING, related_name="pais", blank=False, null=False
+    )
+    departamen = models.ForeignKey(
+        territorial,
+        models.DO_NOTHING,
+        related_name="departamento",
+        blank=False,
+        null=False,
+    )
+    city = models.ForeignKey(
+        territorial, models.DO_NOTHING, related_name="ciudad", blank=False, null=False
+    )
+    category = models.ForeignKey(category, models.DO_NOTHING, blank=False, null=False)
+    user_created = models.ForeignKey(User, models.DO_NOTHING)
+    active = (
+        (
+            1,
+            "activo",
+        ),
+        (
+            0,
+            "inactivo",
+        ),
+    )
+    is_active = models.PositiveSmallIntegerField(
+        max_length=1, choices=active, default=1
+    )
+    created_at = models.DateTimeField(
+        "Fecha de creación", auto_now_add=True, auto_now=False
+    )
+    updated_at = models.DateTimeField(
+        "Fecha de actualización", auto_now_add=False, auto_now=True
+    )
 
-	class Meta:
-		ordering = ['name']
-	
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return "{} {} {}".format(self.id, self.name, self.city)
